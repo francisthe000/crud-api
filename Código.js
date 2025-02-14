91587eb4050c2bab37c5c22b982f932e2984171a
francisthe000 <79719657+francisthe000@users.noreply.github.com>
2025-02-14 08:28:21 -0300

    Update Código.js

--- Código.js
+++ Código.js
@@ -1,198 +1,133 @@
 var gData;
 var url =  'https://script.google.com/macros/s/AKfycby66pO8D9GaWu11-5qgPc6kPO8GVZD3tYtKYkyownmlSxPezRtG2WOH0zdB74qSHKc1/exec' 
 
-  function getDataAPI() {  
-    $('#d_table').html('<div class="col">Please Wait.....</div>') 
-    const url1 = url+'?action=getData'   
-   
-    fetch(url1)
-    .then(response =>{      
-      return response.json()        
+function getDataAPI() {  
+  $('#d_table').html('<div class="col">Please Wait.....</div>') 
+  const url1 = url+'?action=getData'   
+ 
+  fetch(url1)
+  .then(response =>{      
+    return response.json()        
+  })
+  .then(data => {
+    gData = data.data
+    showSendPatientTable(gData)
+    console.log(data.data);
+  })
+  .catch(error => {
+    console.error("Error in fetch:", error.message);
+  });
+}
+
+function addRecord(){
+  document.getElementById("btn1").disabled = true
+  const piso = $('#piso').val()
+  const codigo = $('#codigo').val()
+  const corredor = $('#corredor').val()
+  const prateleira = $('#prateleira').val()
+  const data = $('#data').val()
+  const obj = {
+    piso:piso,
+    codigo:codigo,
+    corredor:corredor,
+    prateleira:prateleira,
+    data:data
+  }
+  const obj_json = JSON.stringify(obj)
+  let formData = new FormData();
+  formData.append('action', 'addData');
+  formData.append('data',obj_json);        
+  fetch(url, {
+    method: 'POST',
+    body: formData,
+  })
+  .then(response => {        
+    return response.json()})
+  .then(data => {
+    console.log(data)        
+    if(data.status == "success"){          
+      document.getElementById("piso").value = "";
+      document.getElementById("codigo").value = "";
+      document.getElementById("corredor").value = "";
+      document.getElementById("prateleira").value = "";
+      document.getElementById("data").value = "";
+      document.getElementById("btn1").disabled = false
+      getDataAPI()
+    }
+  })
+  .catch(error => {
+    console.error(error);
+    document.getElementById("btn1").disabled = false
+  });
+}
+
+function showSendPatientTable(){    
+  if(gData && gData !== undefined && gData.length != 0){
+    let row_number = 0
+    let table_result = `<h5>Edit Table</h5><table class="table table-responsive table-bordered" id="mainTable" style="text-align:center;">
+      <thead>
+        <tr>
+          <th scope='col' class="dt-center">ID</th>
+          <th scope='col' class="dt-center">PISO</th>
+          <th scope='col' class="dt-center">CÓDIGO</th>
+          <th scope='col' class="dt-center">CORREDOR</th>
+          <th scope='col' class="dt-center">PRATELEIRA</th>
+          <th scope='col' class="dt-center">DATA</th>
+          <th scope='col' class="dt-center">AÇÕES</th>
+        </tr>
+      </thead>        
+    `
+    gData.forEach(r =>{
+      table_result += `<tr id="ROWNUMBER:${row_number}">                     
+        <td><input type="hidden" id="idM${row_number}" value="${r['id']}">${r['id']}</td>
+        <td>${r['piso']}</td>
+        <td>${r['codigo']}</td>
+        <td>${r['corredor']}</td>
+        <td>${r['prateleira']}</td>
+        <td>${r['data']}</td>
+        <td>
+          <p style="color:blue;" type="button" onclick="editData(${row_number})"><i class="fa-solid fa-pen-to-square"></i></p>
+          <p style="color:red;" type="button" onclick="delData(${row_number})"><i class="fa-solid fa-trash-can"></i></p>
+        </td>
+      </tr>` 
+      row_number++        
     })
-    .then(data => {
-      gData = data.data
-      showSendPatientTable(gData)
-      console.log(data.data);
-      
-    })
-    .catch(error => {
-      console.error("Error in fetch:", error.message);
-      
+    table_result += "</table>"        
+    $('#d_table').html(table_result)  
+
+    $('#mainTable').DataTable({       
+      lengthMenu: [[20,50,100], [20,50,100]] ,
+      order: [[0, 'asc']],          
     });
+  }else{
+    alert('data not found!')
   }
+}
 
-  function addRecord(){
-    document.getElementById("btn1").disabled = true
-    const name = $('#name').val()
-    const amount = $('#amount').val()
-    const category = $('#category').val()
-    const obj = {
-      name:name,
-      amount:amount,
-      category:category
-    }
-    const obj_json = JSON.stringify(obj)
-    let formData = new FormData();
-      formData.append('action', 'addData');
-      formData.append('data',obj_json);        
-      fetch(url, {
-        method: 'POST',
-        body: formData,
-        //mode:'no-cors'
-      })
-      .then(response => {        
-        return response.json()})
-      .then(data => {
-        console.log(data)        
-        if(data.status == "success"){          
-          document.getElementById("name").value = "";
-          document.getElementById("amount").value = "";
-          document.getElementById("category").value = "";
-          document.getElementById("btn1").disabled = false
-          getDataAPI()
-        }
-             
-      })
-      .catch(error => {
-        console.error(error);
-        document.getElementById("btn1").disabled = false
-      });
-    
-  }
-
-  function showSendPatientTable(){    
-    if(gData && gData !== undefined && gData.length != 0){
-      
-      let row_number = 0
-      let table_result = `<h5>Edit Table</h5><table class="table table-responsive table-bordered" id="mainTable" style="text-align:center;">
-        <thead>
-          <tr>
-            <th scope='col' class="dt-center">Name</th> 
-            <th scope='col' class="dt-center">Amount</th>                             
-            <th scope='col' class="dt-center">Category</th>           
-            <th scope='col' class="dt-center">Action</th> 
-                                
-          </tr>
-        </thead>        
-        `    
-        gData.forEach(r =>{
-        table_result += `<tr id="ROWNUMBER:${row_number}">                     
-            <td><input type="hidden" id="idM${row_number}" value="${r['id']}">${r['name']}</td>   
-            <td>${r['amount']}</td>  
-            <td>${r['category']}</td>    
-            <td>
-              <p style="color:blue;" type="button" onclick="editData(${row_number})"><i class="fa-solid fa-pen-to-square"></i></p>
-              <p style="color:red;" type="button" onclick="delData(${row_number})"><i class="fa-solid fa-trash-can"></i></p>
-            </td>  
-                              
-           </tr>` 
-           row_number++        
-        })
-        table_result += "</table>"        
-        $('#d_table').html(table_result)  
-
+function delData(row_number){
+  const id = $('#idM'+row_number).val()
+  Swal.fire({
+    position: 'center',
+    title: 'Confirm Delete?',         
+    icon: 'warning',          
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'OK',
+    cancelButtonText: 'Cancel'
+  }).then((result) => {
+    if(result.isConfirmed){ 
+      Swal.fire({
+        position: 'center',
+        icon: 'warning',
+        text: "Please Wait...",
+        showConfirmButton: false
+      }) 
+      const obj = { id:id }
+      const obj_json = JSON.stringify(obj)
         
-         $('#mainTable').DataTable({       
-              
-              lengthMenu: [[20,50,100], [20,50,100]] ,
-              order: [[0, 'asc']],          
-              
-         });
-
-         
-      }else{
-        alert('data not found!')
-        
-      }
-  }
-
-  function delData(row_number){
-    const id = $('#idM'+row_number).val()
-    Swal.fire({
-      position: 'center',
-      title: 'Confirm Delete?',         
-      icon: 'warning',          
-      showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'OK',
-          cancelButtonText: 'Cancel'
-      }).then((result) => {
-          if(result.isConfirmed){ 
-              Swal.fire({
-                        position: 'center',
-                        icon: 'warning',
-                        text: "Please Wait...",
-                        showConfirmButton: false                                                    
-                      }) 
-                const obj = {
-                  id:id                       
-                }
-                const obj_json = JSON.stringify(obj)
-                  
-                let formData = new FormData();
-                formData.append('action', 'deleteData');
-                formData.append('data',obj_json);        
-                fetch(url, {
-                     method: 'POST',
-                     body: formData,       
-                })
-                .then(response => {        
-                      return response.json()})
-                .then(data => {
-                     console.log(data)        
-                     if(data.status == "success"){                            
-                         document.getElementById('mainTable').deleteRow(document.getElementById('ROWNUMBER:'+row_number).rowIndex); 
-                         Swal.fire({
-                                position: 'center',
-                                icon: 'success',
-                                text: "Delete!",
-                                timer:2000                     
-                          })
-                      }                               
-                })
-                .catch(error => {
-                  console.error(error);                         
-                });           
-            
-          }//if result.isConfirmed
-      })//then      
-
-
-  }
-  
-  function editData(row_number){
-    
-    const id = $('#idM'+row_number).val()
-    const filterVal = gData.filter(r=>r['id'] === id)
-    const name = filterVal.map(r=>r['name'])[0]
-    const amount = filterVal.map(r=>r['amount'])[0]
-    const category = filterVal.map(r=>r['category'])[0]
-    $('#up_id').val(id)
-    $('#up_name').val(name)
-    $('#up_amount').val(amount)
-    $('#up_category').val(category)
-    $('#edit_form').show()
-    $('#d_table').hide()    
-     
-  }
-
-  function submitEdit(){
-    document.getElementById("btn2").disabled = true
-    const id =  $('#up_id').val()
-    const name = $('#up_name').val()
-    const amount = $('#up_amount').val()
-    const category = $('#up_category').val()
-    const obj = {
-      id:id,
-      name:name,
-      amount:amount,
-      category:category
-    }
-    const obj_json = JSON.stringify(obj)
-
-    let formData = new FormData();
-      formData.append('action', 'editData');
+      let formData = new FormData();
+      formData.append('action', 'deleteData');
       formData.append('data',obj_json);        
       fetch(url, {
         method: 'POST',
@@ -202,38 +137,101 @@ var url =  'https://script.google.com/macros/s/AKfycby66pO8D9GaWu11-5qgPc6kPO8GV
         return response.json()})
       .then(data => {
         console.log(data)        
-        if(data.status == "success"){   
-          
-          document.getElementById("btn2").disabled = false
-          getDataAPI()
-          closeEditEl()
-        }
-             
+        if(data.status == "success"){                            
+          document.getElementById('mainTable').deleteRow(document.getElementById('ROWNUMBER:'+row_number).rowIndex); 
+          Swal.fire({
+            position: 'center',
+            icon: 'success',
+            text: "Deleted!",
+            timer:2000                     
+          })
+        }                               
       })
       .catch(error => {
-        console.error(error);
-        document.getElementById("btn2").disabled = false
-      });
-  }
+        console.error(error);                         
+      });           
+    }//if result.isConfirmed
+  })//then
+}
 
-  function closeEditEl(){
-    $('#up_id').val('')
-    $('#up_name').val('')
-    $('#up_amount').val('')
-    $('#up_category').val('')
-    $('#edit_form').hide()
-    $('#d_table').show()
-  }
-  
+function editData(row_number){
+  const id = $('#idM'+row_number).val()
+  const filterVal = gData.filter(r=>r['id'] === id)
+  const piso = filterVal.map(r=>r['piso'])[0]
+  const codigo = filterVal.map(r=>r['codigo'])[0]
+  const corredor = filterVal.map(r=>r['corredor'])[0]
+  const prateleira = filterVal.map(r=>r['prateleira'])[0]
+  const data = filterVal.map(r=>r['data'])[0]
+  $('#up_id').val(id)
+  $('#up_piso').val(piso)
+  $('#up_codigo').val(codigo)
+  $('#up_corredor').val(corredor)
+  $('#up_prateleira').val(prateleira)
+  $('#up_data').val(data)
+  $('#edit_form').show()
+  $('#d_table').hide()    
+}
 
-  document.getElementById('btn1').addEventListener("click",e=>{
-    addRecord()
+function submitEdit(){
+  document.getElementById("btn2").disabled = true
+  const id =  $('#up_id').val()
+  const piso = $('#up_piso').val()
+  const codigo = $('#up_codigo').val()
+  const corredor = $('#up_corredor').val()
+  const prateleira = $('#up_prateleira').val()
+  const data = $('#up_data').val()
+  const obj = {
+    id:id,
+    piso:piso,
+    codigo:codigo,
+    corredor:corredor,
+    prateleira:prateleira,
+    data:data
+  }
+  const obj_json = JSON.stringify(obj)
+
+  let formData = new FormData();
+  formData.append('action', 'editData');
+  formData.append('data',obj_json);        
+  fetch(url, {
+    method: 'POST',
+    body: formData,       
   })
-  document.getElementById('btn2').addEventListener("click",e=>{
-    submitEdit()
+  .then(response => {        
+    return response.json()})
+  .then(data => {
+    console.log(data)        
+    if(data.status == "success"){   
+      document.getElementById("btn2").disabled = false
+      getDataAPI()
+      closeEditEl()
+    }
   })
-  document.getElementById('btn3').addEventListener("click",e=>{
-    closeEditEl()
-  })
-  
-  document.addEventListener('DOMContentLoaded',getDataAPI)
+  .catch(error => {
+    console.error(error);
+    document.getElementById("btn2").disabled = false
+  });
+}
+
+function closeEditEl(){
+  $('#up_id').val('')
+  $('#up_piso').val('')
+  $('#up_codigo').val('')
+  $('#up_corredor').val('')
+  $('#up_prateleira').val('')
+  $('#up_data').val('')
+  $('#edit_form').hide()
+  $('#d_table').show()
+}
+
+document.getElementById('btn1').addEventListener("click",e=>{
+  addRecord()
+})
+document.getElementById('btn2').addEventListener("click",e=>{
+  submitEdit()
+})
+document.getElementById('btn3').addEventListener("click",e=>{
+  closeEditEl()
+})
+
+document.addEventListener('DOMContentLoaded',getDataAPI)
